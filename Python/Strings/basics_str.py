1c591611e1cd8c5d403218cc4b37ae54f9a3dc65
ayushh <ayushmishra0228@outlook.com>
2025-01-01 13:29:50 +0530

    strings

---
+++ Python/Strings/basics_str.py
@@ -0,0 +1,31 @@
+str = "CholeBhature with Lassi"
+print(len(str)) # prints length of string
+print(str.lower()) # converts string to lower case
+print(str.upper()) # converts string to upper case
+print(str.swapcase()) # swaps case of string
+print(str.title()) # converts string to title case
+print(str.capitalize()) # converts string to capitalize case
+print(str.center(30)) # centers string in 30 characters
+print(str.center(30, '*')) # centers string in 30 characters with '*'
+print(str.ljust(30)) # left justifies string in 30 characters
+print(str.ljust(30, '*')) # left justifies string in 30 characters with '*'
+print(str.rjust(30)) # right justifies string in 30 characters
+print(str.rjust(30, '*')) # right justifies string in 30 characters with '*'
+print(str.zfill(30)) # fills zeros to left of string to make it 30 characters
+print(str.islower()) # checks if string is in lower case
+print(str.isupper()) # checks if string is in upper case
+print(str.istitle()) # checks if string is in title case
+print(str.isalpha()) # checks if string is alphabetic
+print(str.isdigit()) # checks if string is numeric
+print(str.isalnum()) # checks if string is alphanumeric
+print(str.isspace()) # checks if string is space
+print(str.isprintable()) # checks if string is printable
+print(str.startswith("Chole")) # checks if string starts with 'Chole'
+print(str.endswith("Lassi")) # checks if string ends with 'Lassi'
+print(str.count("a")) # counts occurrences of 'a' in string
+
+
+str1 = "he said \" hello \" to me"
+str2 = "Masala \n Dosa"
+print(str1)
+print(str2, end="")
---
+++ Python/Strings/loops_string.py
@@ -0,0 +1,4 @@
+str = 'CholeBhature with Lassi'
+
+for i in str:
+    print(i)  # prints each character of string
