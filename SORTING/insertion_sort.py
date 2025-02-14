047ed34b26c538a6d7d17b22744c663de9765d6c
Ayushh <ayushmishra0228@outlook.com>
2025-02-14 16:34:38 +0530

    sorting

--- SORTING/insertion_sort.py
+++ SORTING/insertion_sort.py
@@ -2,4 +2,11 @@ arr = list(map(int,input("Enter the array:").split()))
 n = len(arr)
 
 def insertionSort(arr):
-    pass
+    for i in range(n): 
+        j = i
+        while(j>0 and arr[j-1]>arr[j]):
+            arr[j-1],arr[j] = arr[j],arr[j-1]
+            j -= 1
+
+insertionSort(arr)
+print(arr)
