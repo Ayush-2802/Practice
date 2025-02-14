df2adc015724b6b346e8176cf94800b2708088d3
Ayushh <ayushmishra0228@outlook.com>
2025-02-14 12:36:11 +0530

    sorting

--- SORTING/selection_sort.py
+++ SORTING/selection_sort.py
@@ -3,12 +3,10 @@ l = len(arr)
 
 def selectionSort(arr):
     for i in range(l):
-        first = i
         min_ele = i
         for j in range(i+1,l):
-            if arr[j] < arr[min_ele]:
-                min_ele = j
-                arr[first] , arr[min_ele] = arr[min_ele] , arr[first]
+            if arr[j] < arr[min_ele]: min_ele = j
+        arr[i] , arr[min_ele] = arr[min_ele] , arr[i]
 
 selectionSort(arr)
 print(arr)
