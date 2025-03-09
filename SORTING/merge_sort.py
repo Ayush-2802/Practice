ff05dd4c981b998f1993230439eb89e13e63364d
Ayushh <ayushmishra0228@outlook.com>
2025-03-10 00:42:32 +0530

    practice merge_sort

--- SORTING/merge_sort.py
+++ SORTING/merge_sort.py
@@ -18,6 +18,7 @@ def merge(arr,low,mid,high):
     while (left<=mid):
         temp.append(arr[left])
         left+=1
+    
     while (right<=high):
         temp.append(arr[right])
         right+=1
@@ -36,3 +37,39 @@ def merge_sort(arr,low,high):
 print(arr)
 merge_sort(arr,0,n-1)
 print(arr)
+
+
+arr = list(map(int,input("Enter the number").split()))
+n = len(arr)
+
+def merge(arr,low,mid,high):
+    temp = []
+    left = low
+    right = mid+1
+
+    while left<=mid and right<=high:
+        if arr[left<=arr[right]]:
+            temp.append(arr[left])
+            left += 1
+        else:
+            temp.append(arr[right])
+            right += 1
+
+    while left<=mid:
+        temp.append(arr[left])
+        left += 1
+
+    while right<=high:
+        temp.append(arr[right])
+        right += 1
+
+    for i in range (len(temp)):
+        arr[low+1] = temp[i]
+
+def merge_sort(arr,low,hig):
+    if low == high : return
+    mid = (low+high)//2
+    merge_sort(arr,low,mid)
+    merge_sort(arr,mid+1,high)
+    merge(arr,low,mid,high)
+
