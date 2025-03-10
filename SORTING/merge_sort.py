bd0d4ca29af5da696f99b18149789bbae0e34d2d
Ayushh <ayushmishra0228@outlook.com>
2025-03-10 18:30:35 +0530

    quick_sort

--- SORTING/merge_sort.py
+++ SORTING/merge_sort.py
@@ -73,3 +73,38 @@ def merge_sort(arr,low,hig):
     merge_sort(arr,mid+1,high)
     merge(arr,low,mid,high)
 
+
+class Solution:
+    
+    def merge(self,arr,low,mid,high):
+        temp = []
+        left = low
+        right = mid+1
+        
+        while left<=mid and right<=high:
+            if left<=low:
+                temp.append(arr[left])
+                left+=1
+            else:
+                temp.append(arr[right])
+                right+=1
+        
+        while left<=mid:
+            temp.append(arr[left])
+            left+=1
+            
+        while right<=high:
+            temp.append(arr[right])
+            right+=1
+        
+        for i in range(len(temp)):
+            arr[i+1] = temp[i]
+            
+            
+    def mergeSort(self,arr, low, high):
+        #code here
+        if low == high : return
+        mid = (low+high)//2
+        mergeSort(arr,low,mid)
+        mergeSort(arr,mid+1,high)
+        merge(arr,low,mid,high)
---
+++ SORTING/quick_sort.py
@@ -0,0 +1,28 @@
+
+arr = list(map(int, input("Enter the array: ").split()))
+n = len(arr)
+
+def part(arr,low,high):
+    pivot = arr[low]
+    i = low
+    j = high
+
+    while(i<j):
+        while (arr[i]<= pivot and i<=high-1):
+            i+=1
+        while (arr[j] >= pivot and j>low):
+            j-=1
+        
+        if i<j :
+            arr[i] , arr[j] = arr[j] , arr[i]
+    arr[low] , arr[j] = arr[j] , arr[low]
+    return j
+
+def quick_sort(arr,low,high):
+    if (low<high):
+        p = part(arr,low,high)
+        quick_sort(arr,low,p-1)
+        quick_sort(arr,p+1,high)
+
+quick_sort(arr,0,n-1)
+print(arr)
