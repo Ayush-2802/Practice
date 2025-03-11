c160b6f3bd8ca24704a28c1dc31a79a288b330a0
Ayushh <ayushmishra0228@outlook.com>
2025-03-11 19:49:27 +0530

    quick_sort

--- SORTING/quick_sort.py
+++ SORTING/quick_sort.py
@@ -28,3 +28,24 @@ def quick_sort(arr,low,high):
 
 quick_sort(arr,0,n-1)
 print(arr)
+
+
+# def part(arr,low,high):
+#     pivot = arr[low]
+#     i = low
+#     j = high
+
+#     while (arr[i]<=pivot and i <=high-1):
+#         i += 1
+#     while (arr[j]>=pivot and j>low):
+#         j -= 1
+#     if i<j:
+#         arr[i],arr[j] = arr[j] , arr[i]
+
+#     arr[low], arr[j] = arr[j] ,arr[low]
+#     return j
+
+# def Quick_sort(arr,low,high):
+#     pind = part(arr,low,high)
+#     quick_sort(arr,low,p-1)
+#     quick_sort(arr,p+1,high)
