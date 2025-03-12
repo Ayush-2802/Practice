1c33ef079909f40a4b76124a040cc3cb01d9f547
Ayushh <ayushmishra0228@outlook.com>
2025-03-12 21:49:28 +0530

    array

--- Array/union_intersection.py
+++ Array/union_intersection.py
@@ -5,3 +5,10 @@ def union(a,b):
     return sorted(set(a)|set(b))
 def intersection(a,b):
     return sorted(set(a)&set(b))
+
+
+def missingNumber(nums: List[int]) -> int:
+        n = len(nums)
+        temp = list(range(n))
+        res = set(temp) - set(nums)
+        return res
