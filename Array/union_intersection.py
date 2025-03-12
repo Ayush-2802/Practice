58974459e1d6a950cfd42c3b0157ff5a0e937988
Ayushh <ayushmishra0228@outlook.com>
2025-03-12 21:32:56 +0530

    array

--- Array/union_intersection.py
+++ Array/union_intersection.py
@@ -1,1 +1,7 @@
-arr = list(map(int,input("enter the array: ").split()))
+a = list(map(int,input("enter the array: ").split()))
+b = list(map(int,input("enter the array: ").split()))
+
+def union(a,b):
+    return sorted(set(a)|set(b))
+def intersection(a,b):
+    return sorted(set(a)&set(b))
