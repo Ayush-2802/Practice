9999ec98f0d1282f27538170fb3ce510915b66b8
Ayushh <ayushmishra0228@outlook.com>
2025-03-14 23:42:29 +0530

    array

---
+++ Array/sortColors.py
@@ -0,0 +1,20 @@
+def sortColors(self, nums: List[int]) -> None:
+    c0 = 0
+    c1 = 0
+    c2 = 0
+    
+    for i in nums:
+        if i == 0:
+            c0 += 1
+        elif i == 1:
+            c1 += 1
+        elif i == 2:
+            c2 += 1
+    
+    # Fill array with correct counts
+    for i in range(0, c0):
+        nums[i] = 0
+    for j in range(c0, c0+c1):
+        nums[j] = 1    # Changed i to j
+    for k in range(c0+c1, len(nums)):
+        nums[k] = 2    # Changed i to k
