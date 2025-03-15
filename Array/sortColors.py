9d13a0a05ccf71ef5178cd9f8355c2dd0e41299c
Ayushh <ayushmishra0228@outlook.com>
2025-03-15 23:26:18 +0530

    array

--- Array/sortColors.py
+++ Array/sortColors.py
@@ -18,3 +18,19 @@ def sortColors(self, nums: List[int]) -> None:
         nums[j] = 1    # Changed i to j
     for k in range(c0+c1, len(nums)):
         nums[k] = 2    # Changed i to k
+
+
+def dutchNationalFlagAlgo(arr,n):
+    low ,mid ,high = 0
+    while(mid<=high):
+        if arr[mid] == 0:
+            arr[low] , arr[mid] = arr[mid] , arr[low]
+            low += 1
+            mid+=1
+
+        elif arr[mid] == 1:
+            mid+=1
+
+        else :
+            arr[mid] , arr[high] = arr[high] ,arr[mid]
+            high -= 1
