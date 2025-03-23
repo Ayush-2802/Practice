e1a1dcf691bafd9677a2513e51312f60e2aebba7
Ayushh <ayushmishra0228@outlook.com>
2025-03-23 16:54:36 +0530

    new

--- Array/SetMatrixZero.py
+++ Array/SetMatrixZero.py
@@ -1,5 +1,6 @@
 mat = [[1,1,1],[1,0,1],[1,1,1]]
 
+#complexity O(n^3)
 def setMatrixZeroBrute(mat):
     n = len(mat)
     m = len(mat[0])
@@ -23,4 +24,22 @@ def setMatrixZeroBrute(mat):
     for i in range(n):
         for j in range(m):
             if mat[i][j] == -1:
+                mat[i][j] = 0
+
+
+def setMatrixZeroBetter(mat):
+    n = len(mat)
+    m = len(mat[0])
+    row = [0]*n
+    col= [0]*m
+
+    for i in range(n):
+        for j in range(m):
+            if mat[i][j] == 0:
+                row[i] = 1
+                col[j] = 1
+            
+    for i in range(n):
+        for j in range(m):
+            if row[i] == 1 or col[j] == 1:
                 mat[i][j] = 0
