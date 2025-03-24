f83ebaf2f5eaa65c7ae4836f58338467335bfa6e
Ayushh <ayushmishra0228@outlook.com>
2025-03-24 12:11:08 +0530

    BS

---
+++ BinarySearch/BSRec.py
@@ -0,0 +1,10 @@
+class BS:
+    def rec(self,nums,target,low,high):
+        if low> high: return -1
+        mid = (low+high)//2
+        if nums[mid] == target: return mid
+        elif target > nums[mid]: return self.rec(nums,target,low,mid-1)
+        else: return self.res(nums,target,mid+1,high)
+
+    def bs(self,nums,target):
+        return self.rec(nums,target,0,len(nums)-1)
