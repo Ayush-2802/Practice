d5e07cb1c79ee92afb0f6a61246e7296ebb32269
Ayushh <ayushmishra0228@outlook.com>
2025-03-24 23:14:44 +0530

    LL

--- Linked list/insertion.py
+++ Linked list/insertion.py
@@ -8,15 +8,21 @@ def InsertionAtFront(head,new_data):
     new.next = head
     return new
 
-def InsertionAtEnd():
-    pass
+def InsertionAtEnd(head,new_data):
+    new = node(new_data)
+    if head is None:
+        return new
+    last = head
+    while last.next:
+        last = last.next
 
-def printList(head):
-    c = head
-    while c is not None:
-        print(c.data,end=" ")
-        c = c.next
-    print()
+    last.next = new
+    return head    
+
+def printList(node):
+    while node:
+        print(node.data,end=" ")
+        node = node.next
 
 if __name__=="__main__":
     head = node(1)
@@ -24,8 +30,16 @@ if __name__=="__main__":
 
     print("Original List: ",end="")
     printList(head)
+    print("")
 
     new_data = 0
     head = InsertionAtFront(head,new_data)
-    print("Updated after Front Insertion: ",end="")
+    print("Updated after Insertion at Front: ",end="")
     printList(head)
+    print("")
+
+    new_data = 3
+    head = InsertionAtEnd(head,new_data)
+    print("Updated after Insertion at End: ",end="")
+    printList(head)
+    print("")
