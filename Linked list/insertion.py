6f53814d856c5587c2e9bdca5df603da3a9a2f64
Ayushh <ayushmishra0228@outlook.com>
2025-03-24 22:57:36 +0530

    LL

---
+++ Linked list/insertion.py
@@ -0,0 +1,28 @@
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+def InsertionAtFront(head,new_data):
+    new = node(new_data)
+    new.next = head
+    return new
+
+def printList(head):
+    c = head
+    while c is not None:
+        print(c.data,end=" ")
+        c = c.next
+    print()
+
+if __name__=="__main__":
+    head = node(1)
+    head.next = node(2)
+
+    print("Original List: ",end="")
+    printList(head)
+
+    new_data = 0
+    head = InsertionAtFront(head,new_data)
+    print("Updated after Front Insertion: ",end="")
+    printList(head)
