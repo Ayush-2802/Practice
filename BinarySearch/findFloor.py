499c3efe31c5c267b8fdf68644d4d5aed15ffe9e
Ayushh <ayushmishra0228@outlook.com>
2025-03-24 19:59:23 +0530

    AlgoUnionFInd

---
+++ BinarySearch/findFloor.py
@@ -0,0 +1,35 @@
+class Solution:
+    #User function Template for python3
+    
+    #Complete this function
+    def findFloor(self, arr, x):
+        #Your code here
+        low = 0
+        high = len(arr) - 1
+        ans = len(arr)
+        
+        while low<=high:
+            mid = low + (high-low) // 2
+            if arr[mid] >= x:
+                ans = mid
+                high = mid-1
+            else:
+                low = mid+1
+        
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        x = int(input().strip())
+        ob = Solution()
+        ans = ob.findFloor(arr, x)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
