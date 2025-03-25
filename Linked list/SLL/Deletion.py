a819f4bec85e3faeeb8abd8867a34c33f4e01bbd
Ayushh <ayushmishra0228@outlook.com>
2025-03-25 11:44:37 +0530

    SLL

--- Linked list/SLL/Deletion.py
+++ Linked list/SLL/Deletion.py
@@ -13,6 +13,40 @@ class Deletion:
         del temp
         return head
     
+    def DelAtLast(head):
+        if not head:
+            return None
+        
+        if not head.next:
+            return Deletion.DelAtFront(head)
+        
+        sec = head
+        while sec.next.next:
+            sec = sec.next
+        
+        sec.next = None
+        return head
+    
+    def DelAtPos(head,pos):
+        if not head:
+            return None
+        
+        # Case 1
+        if pos == 1:
+            return Deletion.DelAtFront(head)
+        
+        # Case 2
+        temp = head
+        prev = None
+        for i in range(1,pos):
+            prev = temp
+            temp = temp.next
+            if temp is None:
+                print("Data Not Present")
+                return head
+        prev.next = temp.next
+        return head
+
 def PrintList(head):
     while head:
         print(head.data,end=" ")
@@ -24,5 +58,5 @@ if __name__=="__main__":
     head.next.next = node(30)
     PrintList(head)
     print("")
-    head = Deletion.DelAtFront(head)
+    head = Deletion.DelAtPos(head,3)
     PrintList(head)
