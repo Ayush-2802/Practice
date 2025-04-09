7cf0acac0371cca4d047e5dfd96c42d9b4492724
ayushh <ayushmishra0228@outlook.com>
2025-04-09 23:02:40 +0530

    leet

--- leetcode Daily/minOperations.py
+++ leetcode Daily/minOperations.py
@@ -9,3 +9,15 @@ class Solution:
             if num > k:
                 count += 1
         return count
+    
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        if min(nums) < k:
+            return -1
+        s = set()
+        for n in nums:
+            if n > k and n not in s:
+                s.add(n)
+        return len(s)
